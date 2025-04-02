a437e655aee639ef3047af6d796fd78cee85cee9
Jose Couso Pérez <146495512+Blayneraptor@users.noreply.github.com>
2025-04-02 11:15:50 +0200

    Añadiendo práctica PHP y MySQL

---
+++ Práctica conexión MYSQL y PHP/inventario_tienda/htdocs/php_inventario/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Inventario Tienda</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h1>Gestión de Inventario</h1>
+    <a href="agregar_producto.php">Agregar Producto</a><br>
+    <a href="listar_productos.php">Ver Productos</a><br>
+    <a href="actualizar_producto.php">Actualizar Producto</a><br>
+    <a href="eliminar_producto.php">Eliminar Producto</a><br>
+</body>
+</html>
